9d411ed59e0150b21bb507525131ab4a00c95ff3
bibo1999 <abdelrahmanmoataz19@gmail.com>
2025-11-08 19:45:31 +0200

    Moving the 3d model, Specify the color onclick by adding scripts in model.jsx

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,17 +3,18 @@ import clsx from 'clsx';
 import {Canvas} from '@react-three/fiber';
 import MacbookModel14 from "./models/Macbook-14";
 import { OrbitControls } from "@react-three/drei";
-import StudioLights from "./StudioLights";
+import StudioLights from "./three/StudioLights";
+import ModelSwitcher from "./three/ModelSwitcher";
+import { useMediaQuery } from 'react-responsive';
 const ProductViewer = () => {
     const {color, scale, setColor, setScale} = useMacbookStore();
-
-
+    const isMobile = useMediaQuery ({ query: '(max-width: 1024px)' });
   return (
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
 
       <div className="controls">
-        <p className="info">MacbookPro {scale} in {color}</p>
+        {/* <p className="info">MacbookPro | Available in 14" & 16" in Space Gray & Dark colors</p> */}
 
         <div className="flex-center gap-5 mt-5">
             <div className="color-control">
@@ -47,9 +48,9 @@ const ProductViewer = () => {
       <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near:0.1, far: 100}}>
         <StudioLights />
 
-        <MacbookModel14 scale={0.06} postion={[0, 0, 0]} />
+        
 
-        <OrbitControls enableZoom={false} />
+        <ModelSwitcher scale= {isMobile ? scale - 0.03 : scale } isMobile={isMobile}/>
       </Canvas>
     </section>
   )
